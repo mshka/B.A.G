29fce9baa8941ea0d5042ee5bcd5c868f3088c04
Boris Paillard <boris.paillard@gmail.com>
2015-11-10 16:41:29 +0200

    finito pepito

--- app/assets/javascripts/side_bar.js
+++ app/assets/javascripts/side_bar.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
   $("#menu-toggle").find("img").attr("src", "/assets/toggle_button_back.png");
   $("#menu-toggle").click(function(e) {
     $("#wrapper").toggleClass("toggled");
+    $(".restaurant-pages").toggleClass("toggled");
     if ($(this).find('img').attr('src') == "/assets/toggle_button.png"){
       $(this).find("img").attr("src", "/assets/toggle_button_back.png");
     }else{
